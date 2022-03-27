53fb5d367dac0de3aa9f383e71e6c8ab1b51e602
aolingpan <aolingpanlx@126.com>
2022-03-27 15:37:02 +0000

    Login, Login Status, Logout, SignUp API

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
@@ -5,4 +6,42 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email']
+        fields = ['id', 'username', 'email']
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=8)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+
+        def validate(self, data):
+            if User.objects.filter(username=data['usename'].lower()).exists():
+                raise ValidationError({
+                    'message': 'This username has been occupied.'
+                })
+            if User.objects.filter(email=data['email'].lower()).exists():
+                raise ValidationError({
+                    'message': 'This email address has been occupied.'
+                })
+            return data
+
+        def create(self, validated_data):
+            username = validated_data['username'].lower()
+            password = validated_data['password']
+            email = validated_data['email'].lower()
+
+            user = User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+            )
+            return user
